5b10c0bfaadca24b9a93acccb33ea6fa4429635c
KJMAN678 <kj.fireman5@gmail.com>
2025-07-14 23:50:50 +0900

    docker-compose.mac.yaml,settings.py:change: PostgreSQLの環境変数設定を外部化し接続情報を動的に設定するように変更

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
